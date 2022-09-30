9d7069e384912f364bcb34b58322bf2e7974ad65
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-09-30 19:55:16 +0400

    tset

---
+++ day_4/switch.dart
@@ -0,0 +1,19 @@
+void main() {
+  int maxNumber = 5;
+  int mediumNumber = 4;
+  int minNumber = 3;
+  int yourNumber = 3;
+  // if (yourNumber > 1) {
+  //   print('Sen agillisan');
+  // }
+  switch (yourNumber) {
+    case 4:
+    case 3:
+      print('senin qiymetin eladir');
+      break;
+
+    default:
+      print('senin qiymetin yoxdur');
+  }
+  void changeNumber() {}
+}
