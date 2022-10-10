ce9af0886bb7eb7fdd66c795eb874c8d5949a57b
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-10-10 19:57:22 +0400

    test

--- exercise/execise.dart
+++ exercise/execise.dart
@@ -1,52 +1,66 @@
-//EXERCISE 
+void main() {
+  //EXERCISE
 //Bir Telebe var ve onu adi Ahmet ,soyadi Tahirli ,yasi 18 atadi Mahirdi.
 //2 il sonra telebenin nece yasi olacaq
 //ekranda telebenin yasi ile onun adinnin uzunluqunun cemini ve ferqini gormek isteyirem
 
-
-//EXERCISE 
-//bir usaqin 30 manat pulu var . Qardasinin ise bundan 20 manat cox pulu var 
+//EXERCISE
+//bir usaqin 30 manat pulu var . Qardasinin ise bundan 20 manat cox pulu var
 //Bu usaqlar birlesib saat almaq isteyir saatin qiymeti 45 manatdir.
 // usaqlarin qalan pullarini goster
 
-
-//EXERCISE 
+//EXERCISE
 // Bir qadini pulu 'Mansur' adli insanin adinin uzluquna beraberdir
 // Qadinin puluna adi 'Sahin' olan adamin adinin uzunluqu qeder pul eleva olunur
 // Qadinin son pulu nedir?
 
-
-//EXERCISE 
+//EXERCISE
 //Senin icinde adalar olan bir cedvelin var ve adlar "mansur",'sahin','tahir','elgun','telman','mahir','xeyyam' dir
 //Men ekranda senin cedvelindeki 4uncu sirada olan adamin adini gormek isteyirem
 
-//EXERCISE 
+//EXERCISE
 //Senin icinde adalar olan bir cedvelin var ve adlar "mansur",'sahin','tahir','elgun','telman','mahir','xeyyam' dir
 //Eger senin listindeki ilk ad mahirdirse ekranda Mahir xos gelmisen yazilsin
 //Eger senin listinin uzunluqu 6dan boyukdursen senin listine yeni bir adam elave olunacaq ve onun adi 'nicat' dir
 
-
-//EXERCISE 
+//EXERCISE
 //Bir adamin 5000 manat pulu var
 //adamin 6 illik xerclerini yazdiqi bir listi var ve onun listindeki reqemler 800,1500,700,900,3000,2500
 //Eger bu adam ne vaxtsa 900 manat pul xecleyibse indiki pulundan hemin 900 manati cixin
 
-
-//EXERCISE 
+//EXERCISE
 //adamin 6 illik xerclerini yazdiqi bir listi var ve onun listindeki reqemler 800,1500,700,900,3000,2500
 //Ekranda adamin 5 il erzinde xerclediyi pulu gormek isteyirem
 //Eger bu adam 5 il erzinde 6000 manatdan cox pul xercleyibse ona sen pulun qedirinin bilmirsen adli melumat ver
 
-
-//EXERCISE 
+//EXERCISE
 //Bir telebe imtahandan 79 bal alir
 //Eger telebe imtahandan 51 den cox 61den az yigsa F,61den cox 71den az yigsa D, 71 den cox 81 den az yigsa C,81 den cox 91den az yigsa B,91den cox yigsa A alacaq
 //Telebenin Herifi qiymeti nedir
-
-
-//EXERCISE 
-//Bir maqazin gunluk olaraq 600 manat alver edir 
-//Vergi sistemine esasende eger bir is yeri gunluk min manatdan cox alver etse ayliq 300 vergi odeyir amma 1000 den az olsa 200 manat odeyir
-//Bu maqazin ne qeder vergi odeyir 
-
-
+  int studentScore = 79;
+  if (studentScore > 51 && studentScore < 61) {
+    print('F');
+  } else if (studentScore > 61 && studentScore < 71) {
+    print('D');
+  } else if (studentScore > 71 && studentScore < 81) {
+    print('C');
+  } else if (studentScore > 81 && studentScore < 91) {
+    print('B');
+  } else {
+    print('A');
+  }
+//EXERCISE
+//Bir maqazin gunluk olaraq 600 manat alver edir
+//Vergi sistemine esasende eger bir is yeri gunluk 1000 manatdan cox alver etse ayliq 300 vergi odeyir amma 1000 den az olsa 200 manat odeyir
+//Bu maqazin ne qeder vergi odeyir
+  int number = 5;
+  int res = number++;
+  int dailyEarn = 600;
+  int maxDailyEarn = 1000;
+  if (dailyEarn > maxDailyEarn) {
+    print();
+    print('Sen ayliq 300 manat vergi odemelisen');
+  } else {
+    print('Sen ayliq 200 manat vergi odemelisen');
+  }
+}
