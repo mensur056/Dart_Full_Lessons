3e66ea22837b406fcf536f0b22054e5a31b06f89
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-10-11 16:58:31 +0400

    test

---
+++ exercise/second_exercise.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+//EXERCISE
+//Bir usaqin 18 yasi var atasinin ise yasi oglunun yasinda 3 defe coxu
+//Eger atanin yasi 60 dan coxdursa ona sen pensiye alacaqsan measi ver azdirsa sizin yasiniz pensiye ucun uygun deyil yaz
+//Eger atanin yasi ile oglunun yasi cemi 72 dirse atanin yasi 2 defe azalsin
+
+//EXERCISE
+//Senin bir listin var  ve o  listde 3 adam adi var 'Cavid','Xeyyam','Elekber'
+//Bu insanlar bir restorana gedirler ve restorana adi yalniz Xeyyam olan insanlar gire bilir
+//Eger senin listinde Xeyyam adli adam varsa yalniz o daxil ola bilecey ve ona ekranda Xeyyam bey xos gelmisiz mesaji verilecey
+
+//EXERCISE
+//Bir telebe ve onun 4 dostu var onlar bir evde qalirlar ve ayliq 500 manat kiraye verirler
+//Telebelerden biri evden cixanda evin pulunun deyeri deyismir diger usaqlar o dostlari cixandan sonra ne qeder pul odemelidirler?
+
+//EXERCISE
+//Bir bank var ve adi PasaBankdir
+//Banka daxil olan insanlar giris ucun 20 manat odemelidirler
+//Bir adamin 30 manat bir qadinin ise 15 manati var
+//Eger iceri daxil ola bilmeyen şexsler var onlara siz iceri gire bilmersiz yazili bir mesaj verilir gire bilen Şexslere ise xos gelmisiz adli bir measj verilir ve pulundan 20 azn cixilir
+
+//EXECISE
+//Insanlar sivitaforda gozleyir
+//Eger qirmizi isiq yansa yolun qarsisina kece bileceyyler
+//Eger yasil yanirsa masinlarin kecmeyini gozlemelidirler
+}
